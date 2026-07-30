8a9fa2c33d9b930dfc3c5c02606c13bbe75a4484
Tishanssr <tishansathruwan2003@gmail.com>
2026-07-30 13:29:12 +0530

    add nodmailer to logic to find secure state base on environment

--- backend/config/nodmailer.js
+++ backend/config/nodmailer.js
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     host:'smtp-relay.brevo.com',
     port:587,
-    secure: false,
+    secure: process.env.NODE_ENV === 'production',
     auth:{
         user:process.env.SMTP_USER,
         pass:process.env.SMTP_PASS
