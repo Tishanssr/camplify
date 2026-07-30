72e34bdb20b6c885ce29ac3278ed2c54f25b90dd
Tishanssr <tishansathruwan2003@gmail.com>
2026-07-30 14:11:07 +0530

    nodemailer edit

--- backend/config/nodmailer.js
+++ backend/config/nodmailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host:'smtp-relay.brevo.com',
-    port:process.env.SMTP_PORT,
+    port:587,
     secure: process.env.NODE_ENV === 'production',
     auth:{
         user:process.env.SMTP_USER,
